3606cfd9958e4591ef1da2cb1b9ee1f82d75a115
Gabriel Waltmann <gabrielwaltmann@gmail.com>
2022-12-13 13:51:52 -0300

    Build: Show calc and result on screen

--- src/Components/Calculator/Index.tsx
+++ src/Components/Calculator/Index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components"
-const Container = styled.section`
+
+const Container = styled.section` //Style
     height: 600px;
     width: 400px;
     display: flex;
@@ -31,12 +32,14 @@ const Container = styled.section`
             font-size: 4rem;
             text-align: end;
             padding-right: 30px;
+            color: #f1f1f1
         }
         .calc{
             height: 100%;
             width: 100%;
             text-align: center;
             font-size: 4rem;
+            color: #f1f1f1
         }
     }
     .keyboard{
@@ -66,129 +69,154 @@ const Container = styled.section`
         }
     }
 `
+
 export function Calculator(){
+    const createButtons = () => { 
+        const datas = [
+            {group: `tool`,
+            name: 'delete',
+            value: null},
+
+            {group: `tool`,
+            name: 'clear',
+            value: null},
+
+            {group: `tool`,
+            name: 'percentage',
+            value: null},
+
+            {group: `operator`,
+            name: 'division',
+            value: `/`},
+
+            {group: `operator`,
+            name: 'multiplication',
+            value: `x`},
+
+            {group: `operator`,
+            name: 'subtraction',
+            value: `-`},
+
+            {group: `operator`,
+            name: 'addition',
+            value: `+`},
+
+            {group: `number`,
+            name: 'number7',
+            value: `7`},
+
+            {group: `number`,
+            name: 'number8',
+            value: `8`},
+
+            {group: `number`,
+            name: 'number9',
+            value: `9`},
+
+            {group: `number`,
+            name: 'number4',
+            value: `4`},
+
+            {group: `number`,
+            name: 'number5',
+            value: `5`},
+
+            {group: `number`,
+            name: 'number6',
+            value: `6`},
+
+            {group: `number`,
+            name: 'number1',
+            value: `1`},
+
+            {group: `number`,
+            name: 'number2',
+            value: `2`},
+
+            {group: `number`,
+            name: 'number3',
+            value: `3`},
+
+            {group: `number`,
+            name: 'number0',
+            value: `0`},
+
+            {group: `number`,
+            name: 'point',
+            value: `.`},
+
+            {group: ``,
+            name: 'result',
+            value: null}
+        ]
+
+        const createButton = (data: any) => {
+            const className = data.group + ` ` + data.name
+            const img = <img src={`src/assets/${data.name}.png`}/>
+            return (
+                <button 
+                className={className} 
+                onClick={()=> updateCalcAndResult(data)}
+                key={className}
+                >
+                    {img}
+                </button>
+            )
+        }
+
+        return datas.map(data => createButton(data))
+    }
+
+    const updateCalcAndResult = (data: any) =>{
+        const group = data.group
+        const value = data.value
+        const name = data.name
+        const functions = {
+            concatenate: () => setCalc(calc+value),
+
+            deleteLastValue: ()=> setCalc(calc.slice(0, -1)),
+
+            clear: ()=> setCalc(calc.replace(calc, ``)),
+
+            showResult: ()=> setResult(eval(calc)),
+
+            addOperator: ()=> {
+                const lastValue = (calc[calc.length-1])
+                if(value != lastValue)setCalc(calc+value)
+                else console.log(lastValue)
+            }
+        }
+
+        if(group == `number`) {
+            functions.concatenate()
+        }
+        else if(name == `delete`) {
+            functions.deleteLastValue()
+        }
+        else if(name == `clear`) {
+            functions.clear()
+        }
+        else if(name == `result`){
+            functions.showResult()
+            functions.clear()
+        }else if(group == `operator`){
+            functions.addOperator()
+        }
+    }
+
+    const [calc, setCalc] = useState(``)
+    const [result, setResult] = useState(0)
+
     return(
         <Container>
             <div className="screen">
                 <div className="history">9 x 15 = 135</div>
-                <div className="calc">9 x 15</div>
-                <div className="result">= 135</div>
+                <div className="calc">{calc}</div>
+                <div className="result">{result}</div>
             </div>
-
             <div className="keyboard">
                 {createButtons()}
             </div>
         </Container>
     )
-}
-function createButtons(){
-    const datas = [
-        {
-        group: `tool`,
-        name: 'delete',
-        value: null
-        },
-        {
-        group: `tool`,
-        name: 'clear',
-        value: null
-        },
-        {
-        group: `tool`,
-        name: 'percentage',
-        value: null
-        },
-        {
-        group: `operator`,
-        name: 'division',
-        value: `/`
-        },
-        {
-        group: `operator`,
-        name: 'multiplication',
-        value: `x`
-        },
-        {
-        group: `operator`,
-        name: 'subtraction',
-        value: `-`
-        },
-        {
-        group: `operator`,
-        name: 'addition',
-        value: `+`
-        },
-        {
-        group: `number`,
-        name: 'number7',
-        value: `7`
-        },
-        {
-        group: `number`,
-        name: 'number8',
-        value: `8`
-        },
-        {
-        group: `number`,
-        name: 'number9',
-        value: `9`
-        },
-        {
-        group: `number`,
-        name: 'number4',
-        value: `4`
-        },
-        {
-        group: `number`,
-        name: 'number5',
-        value: `5`
-        },
-        {
-        group: `number`,
-        name: 'number6',
-        value: `6`
-        },
-        {
-        group: `number`,
-        name: 'number1',
-        value: `1`
-        },
-        {
-        group: `number`,
-        name: 'number2',
-        value: `2`
-        },
-        {
-        group: `number`,
-        name: 'number3',
-        value: `3`
-        },
-        {
-        group: `number`,
-        name: 'number0',
-        value: `0`
-        },
-        {
-        group: `number`,
-        name: 'point',
-        value: `.`
-        },
-        {
-        group: ``,
-        name: 'result',
-        value: null
-        }
-    ]
-
-    function createButton(data: any){
-        const className = data.group + ` ` + data.name
-        const img = <img src={`src/assets/${data.name}.png`}/>
-        return (
-            <button className={className}>
-                {img}
-            </button>
-        )
-    }
-    
-    return datas.map( data => createButton(data))
 }
